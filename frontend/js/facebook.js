efc4570aa3b98abb4eb4084a864c60f815ce58f9
Kashish Bansal <aashma.2008@gmail.com>
2022-03-29 11:52:56 +0530

    added fb authentication

--- frontend/js/facebook.js
+++ frontend/js/facebook.js
@@ -1,9 +1,3 @@
-// These samples are intended for Web so this import would normally be
-// done in HTML however using modules here is more convenient for
-// ensuring sample correctness offline.
-import firebase from "firebase/app";
-import "firebase/auth";
-
 function facebookProvider() {
     // [START auth_facebook_provider_create]
     var provider = new firebase.auth.FacebookAuthProvider();
